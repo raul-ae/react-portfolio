96e0a95787ef71dd0cde1f33024f29734491ce3f
raul-ae <raul.ae@hotmail.com>
2020-09-10 16:57:57 -0500

    Updates

---
+++ precache-manifest.e337b5fec49807be43128f3d02498c32.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f8ff045d0bee3683ab965f502ccf51",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3a411d8ee686ade88de9",
+    "url": "/react-portfolio/static/css/main.2bab676d.chunk.css"
+  },
+  {
+    "revision": "d8d5632f712c5cbb2900",
+    "url": "/react-portfolio/static/js/2.a6c65f0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.a6c65f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a411d8ee686ade88de9",
+    "url": "/react-portfolio/static/js/main.e6145e15.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "141a8f4984c10e68289350bb64ebf7e4",
+    "url": "/react-portfolio/static/media/WikiDrinks.141a8f49.PNG"
+  },
+  {
+    "revision": "f3e425f1ef77ca8df118dc19b9db2d72",
+    "url": "/react-portfolio/static/media/dogeshop.f3e425f1.PNG"
+  },
+  {
+    "revision": "43d3a85327a96732aec1a3684212fd7c",
+    "url": "/react-portfolio/static/media/passw.43d3a853.PNG"
+  },
+  {
+    "revision": "deae9533da6e54062d99c4fc6883a424",
+    "url": "/react-portfolio/static/media/quiz.deae9533.PNG"
+  },
+  {
+    "revision": "ccc7ba07cb566209abc733a8bb7377fd",
+    "url": "/react-portfolio/static/media/scheduler.ccc7ba07.PNG"
+  },
+  {
+    "revision": "3f58bad48434588e461504904ecf65b8",
+    "url": "/react-portfolio/static/media/weather.3f58bad4.PNG"
+  },
+  {
+    "revision": "152fa9c499fcc8b833a7e92fd5f91395",
+    "url": "/react-portfolio/static/media/yo.152fa9c4.png"
+  }
+]);
